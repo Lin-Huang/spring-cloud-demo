e0645c8a1154d47739c5cb438d4f6cb5b2bd1de5
linhuang <1015550661@qq.com>
2018-04-09 22:54:12 +0800

    hystrix监控

--- first/eureka-client/src/main/java/com/lin/client/controller/DcController.java
+++ first/eureka-client/src/main/java/com/lin/client/controller/DcController.java
@@ -5,13 +5,16 @@ import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.TimeUnit;
+
 @RestController
 public class DcController {
     @Autowired
     private DiscoveryClient discoveryClient;
 
     @GetMapping("/dc")
-    public String dc(){
+    public String dc() throws InterruptedException {
+        TimeUnit.SECONDS.sleep(5);
         String service = "Service: " + discoveryClient.getServices();
         System.err.println(service);
         return service;
