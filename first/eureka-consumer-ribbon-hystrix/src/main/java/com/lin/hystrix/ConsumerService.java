857dabc4a01645d57d0a80e0d37240472fc34843
linhuang <1015550661@qq.com>
2018-04-09 18:07:48 +0800

    spring cloud hystrix

---
+++ first/eureka-consumer-ribbon-hystrix/src/main/java/com/lin/hystrix/ConsumerService.java
@@ -0,0 +1,19 @@
+package com.lin.hystrix;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.client.RestTemplate;
+
+public class ConsumerService {
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @HystrixCommand(fallbackMethod = "fallback")
+    public String consumer() {
+        return restTemplate.getForObject("http://eureka-client/dc", String.class);
+    }
+
+    public String fallback() {
+        return "fallback";
+    }
+}
---
+++ first/eureka-consumer-ribbon-hystrix/src/main/java/com/lin/hystrix/DcController.java
@@ -0,0 +1,8 @@
+package com.lin.hystrix;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DcController {
+
+}
