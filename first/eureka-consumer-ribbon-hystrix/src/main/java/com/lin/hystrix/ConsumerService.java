7f9c439c8baa891e0a4774f1eb44f4b3002107d6
linhuang <1015550661@qq.com>
2018-04-09 18:16:42 +0800

    hystrix

--- first/eureka-consumer-ribbon-hystrix/src/main/java/com/lin/hystrix/ConsumerService.java
+++ first/eureka-consumer-ribbon-hystrix/src/main/java/com/lin/hystrix/ConsumerService.java
@@ -2,8 +2,10 @@ package com.lin.hystrix;
 
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
+@Component
 public class ConsumerService {
     @Autowired
     private RestTemplate restTemplate;
--- first/eureka-consumer-ribbon-hystrix/src/main/java/com/lin/hystrix/DcController.java
+++ first/eureka-consumer-ribbon-hystrix/src/main/java/com/lin/hystrix/DcController.java
@@ -1,8 +1,16 @@
 package com.lin.hystrix;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DcController {
+    @Autowired
+    private ConsumerService consumerService;
 
+    @GetMapping("/consumer")
+    public String dc() {
+        return consumerService.consumer();
+    }
 }
